d9ffaf20788530255e4f4a704af598f52c00f496
Endri <61937559+EndriSeferi@users.noreply.github.com>
2020-10-13 01:16:21 +0200

    Updates

---
+++ precache-manifest.3ecca0d5b0e3094d1b5b4e114a6217dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1eb828b92aed48c5087c4f4ded271da",
+    "url": "/endri-seferi/index.html"
+  },
+  {
+    "revision": "4f08adbef1517ce59b87",
+    "url": "/endri-seferi/static/css/main.9d620c7e.chunk.css"
+  },
+  {
+    "revision": "aa8cc5ca392cd853cfab",
+    "url": "/endri-seferi/static/js/2.8d48d59b.chunk.js"
+  },
+  {
+    "revision": "596676716ca1328e2733d607535d6ca7",
+    "url": "/endri-seferi/static/js/2.8d48d59b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f08adbef1517ce59b87",
+    "url": "/endri-seferi/static/js/main.f8f15f6c.chunk.js"
+  },
+  {
+    "revision": "51abbab379b5d5066e1c",
+    "url": "/endri-seferi/static/js/runtime-main.9133be04.js"
+  },
+  {
+    "revision": "8f275e6a87f46c09d1ca886beab33346",
+    "url": "/endri-seferi/static/media/Endri_Seferi_CV.8f275e6a.pdf"
+  }
+]);
